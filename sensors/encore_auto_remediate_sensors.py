f354774281680a4fe378e7ebd4a348dabf6b0b05
shobhitm23 <smakhij@purdue.edu>
2020-07-27 18:23:15 -0400

    added dispatch_trigger and corrected trigger name

--- sensors/encore_auto_remediate_sensors.py
+++ sensors/encore_auto_remediate_sensors.py
@@ -9,6 +9,7 @@ class EncoreAutoRemediateSensor(PollingSensor):
         
         super(EncoreAutoRemediateSensor, self).__init__(sensor_service=sensor_service, config=config, poll_interval=poll_interval)
         self._logger = self._sensor_service.get_logger(__name__)
+        self._trigger_ref = 'foreman.auto_remediate_satellite'
 
     def setup(self):
         USERNAME = self._config['connections']['username']
@@ -50,7 +51,8 @@ class EncoreAutoRemediateSensor(PollingSensor):
                    if actual_time < server_checkin_time:
                        print("This server responded within the query timeframe")
                    else:
-                       print("This server didn't respond since before query time")               
+                       print("This server didn't respond since before query time")
+                       self.dispatch_trigger(obj['certname'], server_checkin_time)               
                    print(new_obj)
                else:
                    print("Subscription facet attributes don't exist")
@@ -58,6 +60,16 @@ class EncoreAutoRemediateSensor(PollingSensor):
        #x = json.dumps(result, indent=4, sort_keys=True)
        
 
+    def dispatch_trigger(self, server_name, last_checkin):
+        trigger = self._trigger_ref
+        
+        payload = {
+            'server_name': server_name,
+            'last_checkin': last_checkin
+        }
+        
+        self.sensor_service.dispatch(trigger=trigger, payload=payload)
+    
     def cleanup(self):
         # This is called when the st2 system goes down. You can perform cleanup operations like
         # closing the connections to external system here.
@@ -74,4 +86,3 @@ class EncoreAutoRemediateSensor(PollingSensor):
     def remove_trigger(self, trigger):
         # This method is called when trigger is deleted
         pass
-
