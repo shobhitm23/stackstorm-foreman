d5174255e94d5b4bcc5aa8fc28e4e0196bfd1042
shobhitm23 <smakhij@purdue.edu>
2020-07-31 14:34:42 -0400

    need syntax change

--- sensors/encore_auto_remediate_sensors.py
+++ sensors/encore_auto_remediate_sensors.py
@@ -4,9 +4,7 @@ from st2reactor.sensor.base import PollingSensor
 from datetime import datetime, timedelta, date
 
 class EncoreAutoRemediateSensor(PollingSensor):
-    
     def __init__(self, sensor_service, config=None, poll_interval=None):
-        
         super(EncoreAutoRemediateSensor, self).__init__(sensor_service=sensor_service, config=config, poll_interval=poll_interval)
         self._logger = self._sensor_service.get_logger(__name__)
         self._trigger_ref = 'foreman.auto_remediate_satellite'
@@ -16,37 +14,28 @@ class EncoreAutoRemediateSensor(PollingSensor):
         PASSWORD = self._config['connections']['password']
         SERVER = self._config['connections']['server']
         SSL_VERIFY = False
-        
         queries = self._config['auto_remediate_sensor_queries']
-        """
-        query_date = self._config['auto_remediate']['last_checkin']['date']
-        query_time = self._config['auto_remediate']['last_checkin']['time']
-        query = query_date + " " + query_time
-        actual_time = datetime.fromisoformat(query)
-        """
-        
+                
     def poll(self):
        session = requests.Session()
        response = session.get(SERVER)
        response.raise_for_status()
        result = response.json()["results"]
-       #final_list = []
     
        for query in queries:
            query_date = query['days']
            timeline = query_date
-	   timeline = int(timeline.split()[0])
+           query_date = int(timeline.split()[0])
            
            for obj in result:
                if "subscription_facet_attributes" in obj:
-
-                   server_checkin_date = obj["subscription_facet_attributes"]["last_checkin"]
+                   server_checkin_date = int(obj["subscription_facet_attributes"]["last_checkin"].split()[0])
                    query_checkin_date = date.today() - timedelta(days=timeline)
                    
-                   if query_checkin_day < server_checkin_date:
+                   if query_checkin_date < server_checkin_date:
                        print("This server responded within the query timeframe")
                    else:
-                       print("This server didn't respond since before query checkin date")
+                       print("This server didn't respond since before query time")
                        self.dispatch_trigger(obj['certname'], server_checkin_date)               
                else:
                    print("Subscription facet attributes don't exist")
